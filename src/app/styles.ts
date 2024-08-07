9b4543ea0211eefdf88de69ba6beec9dadabeffd
Edson Junior <edsonjunior.narvaes@gmail.com>
2024-08-07 01:28:23 -0300

    feat: add important changes and initial page

---
+++ src/app/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const FlexDiv = styled.div`
+  display: flex;
+`;
+
+export const Body = styled(FlexDiv)`
+  font-size: 30px;
+  padding: 80px 40px 40px 40px;
+`;
+
+export const BodyText = styled.text``;
+
+export const NavBar = styled(FlexDiv)`
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 20px 0 20px;
+
+  height: 80px;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: #4d4d4d;
+`;
+
+export const Logo = styled(FlexDiv)``;
+
+export const Menu = styled(FlexDiv)`
+  :hover {
+    color: #4d4d4d;
+  }
+`;
+
+export const FirstNavbarTitle = styled.text`
+  color: #fff;
+
+  margin-right: 24px;
+`;
+
+export const SecondNavbarTitle = styled.text`
+  color: #fff;
+  margin-right: 24px;
+`;
