60517b996c4ad7035cef12a3af0a2c47f505d85a
Edson Junior <edsonjunior.narvaes@gmail.com>
2024-08-08 01:06:08 -0300

    refactor: refactor initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,66 @@
 "use client";
 
 import {
-  Body,
-  BodyText,
-  FirstNavbarTitle,
-  Logo,
-  Menu,
   NavBar,
-  SecondNavbarTitle,
+  Menu,
+  NavbarTitle,
+  Container,
+  Content,
+  BodyDescription,
+  BodyTitle,
+  DescriptionDiv,
+  QuickAccessDiv,
+  Button,
 } from "./styles";
 
 export default function Home() {
   return (
     <>
       <NavBar>
-        <Logo>
-          <svg
-            width="34"
-            height="41"
-            viewBox="0 0 34 41"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.2841 40.5737H8.28036V0.5H10.2841V40.5737ZM6.27668 40.5737H4.27299V0.5H6.27668V40.5737ZM2.26931 40.5737H0.265625V0.5H2.26931V40.5737ZM14.2914 30.5553H33.4656V32.559H14.2914V34.5627H33.4656V36.5664H14.2914V38.57H33.4656V40.5737H12.2877V0.5H33.4656V2.50369H14.2914V4.50737H33.4656V6.51106H14.2914V8.51475H33.4656V10.5184H14.2914V15.4998H32.3525V17.5035H14.2914V19.5072H32.3525V21.5109H14.2914V23.5146H32.3525V25.5183H14.2914V30.5553Z"
-              fill="white"
-            />
-          </svg>
-        </Logo>
         <Menu>
-          <FirstNavbarTitle>Sobre</FirstNavbarTitle>
-          <SecondNavbarTitle>Contato</SecondNavbarTitle>
+          <NavbarTitle>Home</NavbarTitle>
+          <NavbarTitle>Projetos</NavbarTitle>
+          <NavbarTitle>Sobre</NavbarTitle>
+          <NavbarTitle>Contato</NavbarTitle>
         </Menu>
       </NavBar>
-      <Body>
-        <BodyText>
-          Olá, sou Edson Junior. Desenvolvedor mobile, Graduado e Pós-graduado
-          em Desenvolvimento Web e Dispositivos móveis.
-        </BodyText>
-      </Body>
+      <Container>
+        <Content>
+          <BodyTitle>
+            Olá, sou Edson Junior. Desenvolvedor mobile, Graduado e Pós-graduado
+            em Desenvolvimento Web e Dispositivos móveis.
+          </BodyTitle>
+          <DescriptionDiv>
+            <BodyDescription>
+              Iniciei minha carreira como desenvolvedor Front-End, trabalhando
+              com PHP7, Laravel, Slim e Docker. Me especializei em
+              desenvolvimento Web com React.js, Next.js, JavaScript e
+              TypeScript, e participei de um projeto utilizando o Spotify
+              Backstage para gerenciamento de projetos. Atualmente, foco no
+              desenvolvimento mobile com React Native, consumindo APIs REST,
+              utilizando versionamento de código e metodologias ágeis, criando
+              designs no Figma e publicando aplicativos nas lojas Google Play e
+              App Store. Também já atuei como Scrum Master em projetos mobile.
+            </BodyDescription>
+          </DescriptionDiv>
+          <QuickAccessDiv>
+            <Button
+              onClick={() =>
+                window.open("https://github.com/edsonjuniornarvaes", "_blank")
+              }
+            >
+              GitHub
+            </Button>
+            <Button
+              onClick={() =>
+                window.open("https://dev.to/edsonjuniornarvaes", "_blank")
+              }
+            >
+              Dev.to
+            </Button>
+          </QuickAccessDiv>
+        </Content>
+      </Container>
     </>
   );
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -4,41 +4,86 @@ export const FlexDiv = styled.div`
   display: flex;
 `;
 
-export const Body = styled(FlexDiv)`
-  font-size: 30px;
-  padding: 60px 160px 0 160px;
+export const FlexButton = styled.button`
+  display: flex;
 `;
-
-export const BodyText = styled.text``;
 
 export const NavBar = styled(FlexDiv)`
   align-items: center;
-  justify-content: space-between;
-
-  padding: 0 160px 0 160px;
-
+  justify-content: center;
   height: 80px;
-
-  border-bottom-width: 1px;
-  border-bottom-style: solid;
-  border-bottom-color: #4d4d4d;
+  background-color: #171717;
 `;
 
-export const Logo = styled(FlexDiv)``;
+export const NavBarContent = styled(FlexDiv)`
+  width: 100%;
+  max-width: 800px;
+  justify-content: space-around;
+`;
 
-export const Menu = styled(FlexDiv)`
-  :hover {
-    color: #4d4d4d;
+export const NavbarTitle = styled.div`
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  padding: 12px;
+  margin: 0 4px;
+  border-radius: 4px;
+  color: #fff;
+
+  &:hover {
+    background-color: #32312e;
   }
 `;
 
-export const FirstNavbarTitle = styled.text`
-  color: #fff;
-
-  margin-right: 24px;
+export const Menu = styled(FlexDiv)`
+  width: 100%;
+  max-width: 800px;
+  justify-content: space-around;
 `;
 
-export const SecondNavbarTitle = styled.text`
-  color: #fff;
-  margin-right: 24px;
+export const Container = styled(FlexDiv)`
+  height: calc(100vh - 80px);
+  width: 100vw;
+  padding-top: 24px;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  padding: 20px;
+  max-width: 800px;
+  width: 100%;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export const BodyTitle = styled.text`
+  font-weight: 600;
+  font-size: 24px;
+`;
+
+export const DescriptionDiv = styled(FlexDiv)`
+  padding: 24px 0;
+`;
+
+export const BodyDescription = styled.text`
+  font-weight: 200;
+  font-size: 16px;
+`;
+
+export const QuickAccessDiv = styled(FlexDiv)``;
+
+export const Button = styled(FlexButton)`
+  padding: 12px;
+  margin: 0 4px;
+  border-radius: 24px;
+
+  border: #fff;
+  color: #171717;
+  background-color: #fff;
+
+  &:hover {
+    color: #fff;
+    background-color: #32312e;
+  }
 `;
