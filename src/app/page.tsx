e199f2cbc31dfeabcf1bc4b695c3facdef47804d
Edson Junior <edsonjunior.narvaes@gmail.com>
2024-08-09 18:03:23 -0300

    feat: initial page improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,49 @@
 
 import {
   NavBar,
-  Menu,
+  NavbarContent,
   NavbarTitle,
   Container,
   Content,
-  BodyDescription,
-  BodyTitle,
+  ContentDescription,
+  ContentTitle,
   DescriptionDiv,
-  QuickAccessDiv,
-  Button,
+  QuickAccess,
+  QuickAccessButton,
+  ProjectsContent,
+  ProjectsCard,
+  StyledImage,
+  ContentSubtitle,
+  TitleContainer,
+  ProjectsSection,
+  ProjectsSectionTitle,
+  ProjectsCardContainer,
+  ProjectsCardText,
 } from "./styles";
 
 export default function Home() {
   return (
     <>
       <NavBar>
-        <Menu>
+        <NavbarContent>
           <NavbarTitle>Home</NavbarTitle>
           <NavbarTitle>Projetos</NavbarTitle>
           <NavbarTitle>Sobre</NavbarTitle>
           <NavbarTitle>Contato</NavbarTitle>
-        </Menu>
+        </NavbarContent>
       </NavBar>
       <Container>
         <Content>
-          <BodyTitle>
-            Olá, sou Edson Junior. Desenvolvedor mobile, Graduado e Pós-graduado
-            em Desenvolvimento Web e Dispositivos móveis.
-          </BodyTitle>
+          <TitleContainer>
+            <ContentTitle>Olá, eu sou Edson Junior 👋</ContentTitle>
+            <ContentSubtitle>
+              Dev Front-end, Web e Mobile.
+              {/* Desenvolvedor mobile, Graduado e Pós-graduado em Desenvolvimento
+              Web e Dispositivos móveis. */}
+            </ContentSubtitle>
+          </TitleContainer>
           <DescriptionDiv>
-            <BodyDescription>
+            <ContentDescription>
               Iniciei minha carreira como desenvolvedor Front-End, trabalhando
               com PHP7, Laravel, Slim e Docker. Me especializei em
               desenvolvimento Web com React.js, Next.js, JavaScript e
@@ -41,24 +54,47 @@ export default function Home() {
               utilizando versionamento de código e metodologias ágeis, criando
               designs no Figma e publicando aplicativos nas lojas Google Play e
               App Store. Também já atuei como Scrum Master em projetos mobile.
-            </BodyDescription>
+            </ContentDescription>
           </DescriptionDiv>
-          <QuickAccessDiv>
-            <Button
+          <QuickAccess>
+            <QuickAccessButton
               onClick={() =>
                 window.open("https://github.com/edsonjuniornarvaes", "_blank")
               }
             >
-              GitHub
-            </Button>
-            <Button
+              Me acompanhe no GitHub
+            </QuickAccessButton>
+            <QuickAccessButton
               onClick={() =>
                 window.open("https://dev.to/edsonjuniornarvaes", "_blank")
               }
             >
-              Dev.to
-            </Button>
-          </QuickAccessDiv>
+              Me acompanhe no Dev.to
+            </QuickAccessButton>
+          </QuickAccess>
+          <ProjectsSection>
+            <ProjectsSectionTitle>ÚLTIMOS DROPS</ProjectsSectionTitle>
+          </ProjectsSection>
+          <ProjectsContent>
+            <ProjectsCard>
+              <StyledImage
+                src="https://img.freepik.com/fotos-gratis/computador-laptop-cinza-ligado_400718-47.jpg?t=st=1723237356~exp=1723240956~hmac=4eafc2f572d1489078d27db4d7ca5592e9fd69d0a78b7929d233c3bc7c336362&w=740"
+                alt="Exemplo de Imagem"
+              />
+              <ProjectsCardContainer>
+                <ProjectsCardText>Projetos</ProjectsCardText>
+              </ProjectsCardContainer>
+            </ProjectsCard>
+            <ProjectsCard>
+              <StyledImage
+                src="https://img.freepik.com/fotos-gratis/vista-da-configuracao-da-mesa-de-jogos-iluminada-a-neon-com-teclado_23-2149529348.jpg?t=st=1723235991~exp=1723239591~hmac=d690003c86b97b0a6af32bde788497e4b21df90025ae373d5f72941b21c96472&w=740"
+                alt="Exemplo de Imagem"
+              />
+              <ProjectsCardContainer>
+                <ProjectsCardText>Stack</ProjectsCardText>
+              </ProjectsCardContainer>
+            </ProjectsCard>
+          </ProjectsContent>
         </Content>
       </Container>
     </>
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -11,13 +11,16 @@ export const FlexButton = styled.button`
 export const NavBar = styled(FlexDiv)`
   align-items: center;
   justify-content: center;
+
   height: 80px;
+
   background-color: #171717;
 `;
 
 export const NavBarContent = styled(FlexDiv)`
   width: 100%;
   max-width: 800px;
+
   justify-content: space-around;
 `;
 
@@ -25,9 +28,12 @@ export const NavbarTitle = styled.div`
   font-weight: 400;
   font-size: 0.875rem;
   line-height: 1.25rem;
-  padding: 12px;
+
   margin: 0 4px;
+  padding: 12px;
+
   border-radius: 4px;
+
   color: #fff;
 
   &:hover {
@@ -35,55 +41,173 @@ export const NavbarTitle = styled.div`
   }
 `;
 
-export const Menu = styled(FlexDiv)`
+export const NavbarContent = styled(FlexDiv)`
+  align-items: center;
+  justify-content: center;
+  /* justify-content: center; */
+
+  gap: 24px;
+  /* justify-content: space-around; */
+
   width: 100%;
   max-width: 800px;
-  justify-content: space-around;
 `;
 
 export const Container = styled(FlexDiv)`
-  height: calc(100vh - 80px);
-  width: 100vw;
-  padding-top: 24px;
   align-items: flex-start;
   justify-content: center;
+
+  width: 100vw;
+  height: calc(100vh - 80px);
+  padding: 60px 0 24px 0;
 `;
 
 export const Content = styled.div`
-  padding: 20px;
-  max-width: 800px;
+  align-items: center;
+  justify-content: center;
+
   width: 100%;
+  max-width: 800px;
+
+  /* padding: 20px; */
+
   border-radius: 12px;
+
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `;
 
-export const BodyTitle = styled.text`
+export const Title = styled.text`
   font-weight: 600;
-  font-size: 24px;
+
+  /* font-size: 32px; */
+  font-size: 2.25rem;
+`;
+
+export const TitleContainer = styled(FlexDiv)`
+  /* background-color: green; */
+  flex-direction: column;
+
+  /* text-align: center; */
+
+  padding-bottom: 12px;
+`;
+
+export const ContentTitle = styled(Title)`
+  font-weight: 700;
+`;
+
+export const ContentSubtitle = styled(Title)`
+  font-weight: 700;
+
+  margin-top: 12px;
 `;
 
 export const DescriptionDiv = styled(FlexDiv)`
-  padding: 24px 0;
+  /* background-color: red; */
+  padding: 12px 0;
+
+  /* text-align: center; */
 `;
 
-export const BodyDescription = styled.text`
+export const ContentDescription = styled.text`
   font-weight: 200;
-  font-size: 16px;
+
+  /* font-size: 16px; */
+  font-size: 1.125rem;
 `;
 
-export const QuickAccessDiv = styled(FlexDiv)``;
+export const QuickAccess = styled(FlexDiv)`
+  align-items: center;
+  /* justify-content: center; */
 
-export const Button = styled(FlexButton)`
+  /* background-color: yellow; */
+  padding: 48px 0;
+`;
+
+export const QuickAccessButton = styled(FlexButton)`
   padding: 12px;
   margin: 0 4px;
-  border-radius: 24px;
 
-  border: #fff;
   color: #171717;
   background-color: #fff;
+
+  border: #fff;
+  border-radius: 24px;
 
   &:hover {
     color: #fff;
     background-color: #32312e;
   }
+`;
+
+export const ProjectsContent = styled(FlexDiv)`
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 24px;
+
+  padding-bottom: 24px;
+`;
+
+export const ProjectsCard = styled(FlexDiv)`
+  position: relative;
+  flex-direction: column;
+
+  flex: 1 1 300px;
+  min-width: 250px;
+  max-width: 390px;
+
+  /* padding: 16px; */
+  /* box-sizing: border-box; */
+
+  /* border-top-left-radius: 12px; */
+  /* border-top-right-radius: 12px; */
+`;
+
+export const ProjectsSection = styled(FlexDiv)`
+  align-items: center;
+  /* background-color: red; */
+
+  padding-bottom: 24px;
+
+  /* justify-content: center; */
+`;
+
+export const ProjectsSectionTitle = styled.text`
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  font-weight: 700;
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: 180px; /* Define uma altura fixa */
+  object-fit: cover; /* Mantém a proporção da imagem e preenche o espaço */
+  /* border-top-left-radius: 12px;
+  border-top-right-radius: 12px; */
+
+  border-radius: 12px;
+
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+
+  opacity: 0.8; /* Define a opacidade do componente */
+  background-color: #f0f0f0; /* Exemplo de cor de fundo */
+
+  transition: transform 0.3s ease-in-out; /* Suaviza a transformação */
+
+  &:hover {
+    transform: scale(1.05); /* Aumenta o componente em 5% ao passar o mouse */
+  }
+`;
+
+export const ProjectsCardContainer = styled(FlexDiv)`
+  position: absolute;
+  bottom: 12px;
+  padding-left: 12px;
+`;
+
+export const ProjectsCardText = styled.text`
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+
+  color: #fff;
 `;
