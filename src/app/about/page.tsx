1c914528747890bf5870bb190a3141eff13107b7
Edson Junior <edsonjunior.narvaes@gmail.com>
2024-08-12 00:06:33 -0300

    style: add improvements

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,22 @@
 "use client";
 
+import Container from "../components/container";
 import * as S from "../styles";
 
 const About = () => {
   return (
-    <S.Container>
-      <S.Content>
-        <S.ContentDescription>
-          Iniciei minha carreira como desenvolvedor Front-End, trabalhando com
-          PHP7, Laravel, Slim e Docker. Me especializei em desenvolvimento Web
-          com React.js, Next.js, JavaScript e TypeScript, e participei de um
-          projeto utilizando o Spotify Backstage para gerenciamento de projetos.
-          Atualmente, foco no desenvolvimento mobile com React Native,
-          consumindo APIs REST, utilizando versionamento de código e
-          metodologias ágeis, criando designs no Figma e publicando aplicativos
-          nas lojas Google Play e App Store. Também já atuei como Scrum Master
-          em projetos mobile.
-        </S.ContentDescription>
-      </S.Content>
-    </S.Container>
+    <Container>
+      <S.ContentDescription>
+        Iniciei minha carreira como desenvolvedor Front-End, trabalhando com
+        PHP7, Laravel, Slim e Docker. Me especializei em desenvolvimento Web com
+        React.js, Next.js, JavaScript e TypeScript, e participei de um projeto
+        utilizando o Spotify Backstage para gerenciamento de projetos.
+        Atualmente, foco no desenvolvimento mobile com React Native, consumindo
+        APIs REST, utilizando versionamento de código e metodologias ágeis,
+        criando designs no Figma e publicando aplicativos nas lojas Google Play
+        e App Store. Também já atuei como Scrum Master em projetos mobile.
+      </S.ContentDescription>
+    </Container>
   );
 };
 
